f48248877b57e75f837d26eb4dff6ca031169837
myintmyatthein <myintmyatthein403@gmail.com>
2025-03-04 10:00:45 +0700

    Update Daily Challenges
    Medium

---
+++ Medium/2161. Partition Array According to Given Pivot/Python/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        left = [];
+        middle = [];
+        right = [];
+        for i in range(len(nums)):
+            if nums[i] < pivot:
+                left.append(nums[i])
+            elif nums[i] == pivot:
+                middle.append(nums[i])
+            else:
+                right.append(nums[i])
+        return left + middle + right
+
+
